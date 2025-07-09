f6ee3c68476c7abd7380a3ef39eca1e44d764423
Ian-Protocol <ipascal@live.ca>
2025-07-09 14:47:06 -0500

    Create customer validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,8 @@
 class Customer < ApplicationRecord
+    has_one_attached :image
+
+    validates :full_name, presence: true
+
+    # https://stackoverflow.com/questions/38611405/email-validation-in-ruby-on-rails
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
 end
