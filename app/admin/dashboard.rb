22f363f07cb7acd2ace80e5b921abb9c43a2875d
Ian-Protocol <ipascal@live.ca>
2025-07-09 14:19:22 -0500

    Edit default AA dashboard to show customers instead of posts

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -12,22 +12,22 @@ ActiveAdmin.register_page "Dashboard" do
 
     # Here is an example of a simple dashboard with columns and panels.
     #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    columns do
+      column do
+        panel "Recent Customers" do
+          ul do
+            Customer.order(created_at: :desc).limit(5).map do |customer|
+              li link_to("#{customer.full_name} (#{customer.email_address})", admin_customer_path(customer))
+            end
+          end
+        end
+      end
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
+      column do
+        panel "Info" do
+          para "Welcome to ActiveAdmin. Hi Mom!"
+        end
+      end
+    end
   end # content
 end
