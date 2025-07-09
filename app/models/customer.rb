9b340e4796f300ce12f92200e341594c7224628b
Ian-Protocol <ipascal@live.ca>
2025-07-09 15:35:03 -0500

    Fix Ransack errors

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,5 +4,14 @@ class Customer < ApplicationRecord
     validates :full_name, presence: true
 
     # https://stackoverflow.com/questions/38611405/email-validation-in-ruby-on-rails
-    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+    # From Ransack errors:
+    def self.ransackable_associations(auth_object = nil)
+        ["image_attachment", "image_blob"]
+    end
+
+    def self.ransackable_attributes(auth_object = nil)
+        ["created_at", "email_address", "full_name", "id", "notes", "phone_number", "updated_at"]
+    end
 end
